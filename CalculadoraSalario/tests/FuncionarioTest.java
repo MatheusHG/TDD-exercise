bb53f3bc705148246406f706229165c577934d09
André Martins <andre.lucas.medeir@gmail.com>
2023-04-12 11:17:43 -0300

    refact: ajustando testes para a nova funcionalidade dentro de cargo

--- CalculadoraSalario/tests/FuncionarioTest.java
+++ CalculadoraSalario/tests/FuncionarioTest.java
@@ -23,21 +23,92 @@ public class FuncionarioTest {
     String email = "andre.martins@ccc.ufcg.edu.br";
     Cargo cargo = Cargo.DESENVOLVEDOR;
 
-    double _10PERCENT = 0.1;
-    double _20PERCENT = 0.2;
+    double salarioMenor = 2999;
+    double salarioIgual = 3000;
+    double salarioMaior = 3001;
     
-    double salarioMenor = 2999.99;
-    double salarioIgual = 3000.00;
-    double salarioMaior = 3000.01;
+    double salarioMenorComDescontoMenor = salarioMenor - (salarioMenor * 0.10);
+    double salarioIgualComDescontoMaior = salarioIgual - (salarioIgual * 0.20);
+    double salarioMaiorComDescontoMaior = salarioMaior - (salarioMaior * 0.20);
 
-    Funcionario funcionarioMenor3000 = new Funcionario(nome, email, salarioMenor, cargo);
-    Funcionario funcionarioIgual3000 = new Funcionario(nome, email, salarioIgual, cargo);
-    Funcionario funcionarioMaior3000 = new Funcionario(nome, email, salarioMaior, cargo);
+    Funcionario funcionarioMenor = new Funcionario(nome, email, salarioMenor, cargo);
+    Funcionario funcionarioIgual = new Funcionario(nome, email, salarioIgual, cargo);
+    Funcionario funcionarioMaior = new Funcionario(nome, email, salarioMaior, cargo);
 
-    assert(salarioMenor - (salarioMenor * _10PERCENT) == funcionarioMenor3000.calculaSalario());
-
-    assert(salarioIgual - (salarioIgual * _20PERCENT) == funcionarioIgual3000.calculaSalario());
-
-    assert(salarioMaior - (salarioMaior * _20PERCENT) == funcionarioMaior3000.calculaSalario());
+    assert(salarioMenorComDescontoMenor == funcionarioMenor.calculaSalario());
+    assert(salarioIgualComDescontoMaior == funcionarioIgual.calculaSalario());
+    assert(salarioMaiorComDescontoMaior == funcionarioMaior.calculaSalario());
   }
+
+  @Test
+  public void testCalcularSalarioDBA() {
+    String nome = "André";
+    String email = "andre.martins@ccc.ufcg.edu.br";
+    Cargo cargo = Cargo.DBA;
+
+    double salarioMenor = 1999;
+    double salarioIgual = 2000;
+    double salarioMaior = 2001;
+    
+    double salarioMenorComDescontoMenor = salarioMenor - (salarioMenor * 0.15);
+    double salarioIgualComDescontoMaior = salarioIgual - (salarioIgual * 0.25);
+    double salarioMaiorComDescontoMaior = salarioMaior - (salarioMaior * 0.25);
+
+    Funcionario funcionarioMenor = new Funcionario(nome, email, salarioMenor, cargo);
+    Funcionario funcionarioIgual = new Funcionario(nome, email, salarioIgual, cargo);
+    Funcionario funcionarioMaior = new Funcionario(nome, email, salarioMaior, cargo);
+
+    System.out.println(funcionarioMenor.calculaSalario());
+
+    assert(salarioMenorComDescontoMenor == funcionarioMenor.calculaSalario());
+    assert(salarioIgualComDescontoMaior == funcionarioIgual.calculaSalario());
+    assert(salarioMaiorComDescontoMaior == funcionarioMaior.calculaSalario());
+  }
+
+  @Test
+  public void testCalcularSalarioTestador() {
+    String nome = "André";
+    String email = "andre.martins@ccc.ufcg.edu.br";
+    Cargo cargo = Cargo.TESTADOR;
+
+    double salarioMenor = 1999;
+    double salarioIgual = 2000;
+    double salarioMaior = 2001;
+    
+    double salarioMenorComDescontoMenor = salarioMenor - (salarioMenor * 0.15);
+    double salarioIgualComDescontoMaior = salarioIgual - (salarioIgual * 0.25);
+    double salarioMaiorComDescontoMaior = salarioMaior - (salarioMaior * 0.25);
+
+    Funcionario funcionarioMenor = new Funcionario(nome, email, salarioMenor, cargo);
+    Funcionario funcionarioIgual = new Funcionario(nome, email, salarioIgual, cargo);
+    Funcionario funcionarioMaior = new Funcionario(nome, email, salarioMaior, cargo);
+
+    assert(salarioMenorComDescontoMenor == funcionarioMenor.calculaSalario());
+    assert(salarioIgualComDescontoMaior == funcionarioIgual.calculaSalario());
+    assert(salarioMaiorComDescontoMaior == funcionarioMaior.calculaSalario());
+  }
+
+  @Test
+  public void testCalcularSalarioGerente() {
+    String nome = "André";
+    String email = "andre.martins@ccc.ufcg.edu.br";
+    Cargo cargo = Cargo.GERENTE;
+
+    double salarioMenor = 4999;
+    double salarioIgual = 5000;
+    double salarioMaior = 5001;
+    
+    double salarioMenorComDescontoMenor = salarioMenor - (salarioMenor * 0.20);
+    double salarioIgualComDescontoMaior = salarioIgual - (salarioIgual * 0.30);
+    double salarioMaiorComDescontoMaior = salarioMaior - (salarioMaior * 0.30);
+
+    Funcionario funcionarioMenor = new Funcionario(nome, email, salarioMenor, cargo);
+    Funcionario funcionarioIgual = new Funcionario(nome, email, salarioIgual, cargo);
+    Funcionario funcionarioMaior = new Funcionario(nome, email, salarioMaior, cargo);
+
+    assert(salarioMenorComDescontoMenor == funcionarioMenor.calculaSalario());
+    assert(salarioIgualComDescontoMaior == funcionarioIgual.calculaSalario());
+    assert(salarioMaiorComDescontoMaior == funcionarioMaior.calculaSalario());
+  }
+
 }
