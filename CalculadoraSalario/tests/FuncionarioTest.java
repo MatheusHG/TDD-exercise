6b0dbd537843f4b040b656cf0c84fe255f61ac7e
André Martins <andre.lucas.medeir@gmail.com>
2023-04-12 09:59:41 -0300

    feat: adicionado teste de calcular salário

--- CalculadoraSalario/tests/FuncionarioTest.java
+++ CalculadoraSalario/tests/FuncionarioTest.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class FuncionarioTest {
@@ -15,5 +17,41 @@ public class FuncionarioTest {
     assert(funcionario.getEmail().equals(email));
     assert(funcionario.getSalarioBase() == (salarioBase));
     assert(funcionario.getCargo().equals(cargo));
+  }
+
+  @Test
+  public void testCalcularSalarioDesenvolvedor() {
+    String nome = "André";
+    String email = "andre.martins@ccc.ufcg.edu.br";
+    Cargo cargo = Cargo.DESENVOLVEDOR;
+
+    double _10PERCENT = 0.1;
+    double _20PERCENT = 0.2;
+    
+    double salarioMenor = 2999.99;
+    double salarioIgual = 3000.00;
+    double salarioMaior = 3000.01;
+
+    Funcionario funcionarioMenor3000 = new Funcionario(nome, email, salarioMenor, cargo);
+    Funcionario funcionarioIgual3000 = new Funcionario(nome, email, salarioIgual, cargo);
+    Funcionario funcionarioMaior3000 = new Funcionario(nome, email, salarioMaior, cargo);
+
+    assertEquals(
+      "Calcula salário de funcionario com salario base menor que 3000",
+      salarioMenor - (salarioMenor * _10PERCENT),
+      funcionarioMenor3000.calculaSalario()
+    );
+
+    assertEquals(
+      "Calcula salário de funcionario com salario base igual a 3000",
+      salarioIgual - (salarioIgual * _10PERCENT),
+      funcionarioIgual3000.calculaSalario()
+    );
+
+    assertEquals(
+      "Calcula salário de funcionario com salario base maior que 3000",
+      salarioMaior - (salarioMaior * _10PERCENT),
+      funcionarioMaior3000.calculaSalario()
+    );
   }
 }
