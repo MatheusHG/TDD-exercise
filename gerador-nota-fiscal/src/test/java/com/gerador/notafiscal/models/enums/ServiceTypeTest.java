59ce831364d77fed578c516735f10aee310436c3
MatheusHG <matheus.oliveira@ccc.ufcg.edu.br>
2023-04-13 23:18:24 -0300

    add test service type

--- gerador-nota-fiscal/src/test/java/com/gerador/notafiscal/models/enums/ServiceTypeTest.java
+++ gerador-nota-fiscal/src/test/java/com/gerador/notafiscal/models/enums/ServiceTypeTest.java
@@ -9,6 +9,46 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class ServiceTypeTest {
+    @Test
+    public void shouldGetServiceTypeFromDescription() {
+        String description = "Assessoria";
+        ServiceType serviceType = ServiceType.getFromDescription(description);
+
+        assertEquals(description, serviceType.getDescription());
+    }
+
+    @Test
+    public void shouldGetServiceTypeFromTrainingDescription() {
+        String description = "Treinamento";
+        ServiceType serviceType = ServiceType.getFromDescription(description);
+
+        assertEquals(description, serviceType.getDescription());
+    }
+
+    @Test
+    public void shouldGetServiceTypeFromOtherDescription() {
+        String description = "Outro";
+        ServiceType serviceType = ServiceType.getFromDescription(description);
+
+        assertEquals(description, serviceType.getDescription());
+    }
+
+    @Test
+    public void shouldGetServiceTypeFromUnknownDescription() {
+        String description = "Qualquer tipo";
+        ServiceType serviceType = ServiceType.getFromDescription(description);
+
+        assertEquals("Outro", serviceType.getDescription());
+    }
+
+    @Test
+    public void shouldGetListOfAllServiceTypeDescriptions() {
+        List<String> expected = Arrays.asList("Assessoria", "Treinamento", "Outro");
+        List<String> generatedList = ServiceType.getDescriptions();
+
+        assertEquals(expected, generatedList);
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrownErrorForNullDescription() {
         ServiceType.getFromDescription(null);
