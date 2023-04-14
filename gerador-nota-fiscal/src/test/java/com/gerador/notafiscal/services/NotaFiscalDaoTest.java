c7c56bb11c50a79220dfe842c9ff68c0278b06a9
MatheusHG <matheus.oliveira@ccc.ufcg.edu.br>
2023-04-13 23:14:54 -0300

    test notaFiscalDaoTest

---
+++ gerador-nota-fiscal/src/test/java/com/gerador/notafiscal/services/NotaFiscalDaoTest.java
@@ -0,0 +1,44 @@
+package com.gerador.notafiscal.services;
+
+import com.gerador.notafiscal.models.Bill;
+import com.gerador.notafiscal.models.NotaFiscal;
+import com.gerador.notafiscal.models.enums.ServiceType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class NotaFiscalDaoTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    public NotaFiscal notaFiscal;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Before
+    public void setup() {
+        Bill bill = new Bill("Matheus Oliveira", "Rua do Sol, 89", ServiceType.CONSULTING, 1000.0);
+        this.notaFiscal = new NotaFiscal(bill);
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void shouldHaveNoErrorsOnSave() {
+        NotaFiscalDao notaFiscalDao = new NotaFiscalDao();
+        notaFiscalDao.saveToDB(notaFiscal);
+
+        assertEquals("Saving customer Matheus Oliveira's invoice in the database...\n", outContent.toString());
+    }
+}
